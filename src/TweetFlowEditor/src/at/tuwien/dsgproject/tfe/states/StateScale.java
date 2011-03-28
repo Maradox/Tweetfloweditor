29d05626dcc429ffc1291d7050b657170f223e7d
Matthias Neumayr <matt.neumayr@gmail.com>
2011-03-28 17:10:00 +0200

    added StateScale and proper handling of scaling gestures

---
+++ src/TweetFlowEditor/src/at/tuwien/dsgproject/tfe/states/StateScale.java
@@ -0,0 +1,12 @@
+package at.tuwien.dsgproject.tfe.states;
+
+import at.tuwien.dsgproject.tfe.views.EditorView;
+
+public class StateScale extends State {
+
+	public StateScale(EditorView editorView) {
+		super(editorView);
+		// TODO Auto-generated constructor stub
+	}
+
+}
